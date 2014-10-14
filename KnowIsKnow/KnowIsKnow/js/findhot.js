2aef329d4f1a20f4a9d65b65bb17fcd851a2826b
hytczhoumingxing <1027883520@qq.com>
2014-10-14 15:59:05 +0800

    2014-10-14
    初步完成发现页面

--- KnowIsKnow/KnowIsKnow/js/findhot.js
+++ KnowIsKnow/KnowIsKnow/js/findhot.js
@@ -1,3 +1,44 @@
 ﻿$(function () {
-    alert(123);
+
+    $(".zmxtab-nav").click(function () {
+        $(".zmxtab-nav").removeClass("active");
+
+        $(this).addClass("active");
+
+    });
+    $("#zmxtabpanelmou").css("display", "none");
+    $(".zmxhot").click(function () {
+        $(".zmxtab-panel").css("display", "block");
+        $("#zmxtabpanelmou").css("display", "none");
+    })
+    $(".zmxmhot").click(function () {
+        $(".zmxtab-panel").css("display", "none");
+        $("#zmxtabpanelmou").css("display", "block");
+    })
+    $(".zmxpanel").hover(function () {
+        $(".zmxpanel3").show();
+        $(".zmxpanel4").show();
+        $(".zmxpanel5").show();
+        $(".zmxpanel6").show();
+        $(".zmxdian").show();
+
+    }, function () {
+        $(".zmxpanel3").hide();
+        $(".zmxpanel4").hide();
+        $(".zmxpanel5").hide();
+        $(".zmxpanel6").hide();
+        $(".zmxdian").hide();
+    });
+    $(".zmxex").click(function () {
+        $(".zmxshouqi").css("display", "block");
+        $(".zmxhidecontent").css("display", "block");
+        $(".zmxdate").css("display", "block");
+        $(".zmxrich").css("display", "none");
+    })
+    $(".zmxcollapse").click(function () {
+        $(".zmxshouqi").css("display", "none");
+        $(".zmxhidecontent").css("display", "none");
+        $(".zmxdate").css("display", "none");
+        $(".zmxrich").css("display", "block");
+    })
 });
