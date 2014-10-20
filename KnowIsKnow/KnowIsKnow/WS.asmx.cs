a471318ccd3630c76de7107e49a2a0ce4337a355
916435526 <349712821@qq.com>
2014-10-20 16:51:51 +0800

    关注话题修正版

--- KnowIsKnow/KnowIsKnow/WS.asmx.cs
+++ KnowIsKnow/KnowIsKnow/WS.asmx.cs
@@ -38,7 +38,7 @@ namespace KnowIsKnow
             return "ok";
         }
         [WebMethod]
-<<<<<<< HEAD
+
         public List<Model.UserInfo> selectUserInfo(string usernickname ) 
         {
             BLL.UserInfo userinfo = new BLL.UserInfo();
@@ -47,51 +47,27 @@ namespace KnowIsKnow
             
             return da; 
         }
-        [WebMethod]
-        public void AddCareTopic(string topicid,string userid)
-=======
-        public string selectUserInfo(string usernickname)
-        {
-            BLL.UserInfo userinfo = new BLL.UserInfo();
-            DataSet da = userinfo.GetList("userNickName like '%" + usernickname + "%'");
 
-            return da.Tables[0].Rows[0]["userNickName"].ToString(); ;
-        }
+       
         [WebMethod]
         public string AddCareTopic(string topicid, string userid)
->>>>>>> origin/master
         {
             BLL.CareTopic topic = new BLL.CareTopic();
             Model.CareTopic topicinfo = new Model.CareTopic();
             topicinfo.careTopicID = Convert.ToInt32(topicid);
-<<<<<<< HEAD
+
             topicinfo.topicCaredByUID = Convert.ToInt32( userid);
         
             topic.Add(topicinfo);
-            
-        }
-        [WebMethod]
-        public  void DeleteCareTopic(int topicid, int userid)
-        {
-            BLL.CareTopic topic = new BLL.CareTopic();
-            Model.CareTopic topicinfo = new Model.CareTopic();
-            topic.Delete(topicid,userid);
-            
-=======
-            topicinfo.topicCaredByUID = Convert.ToInt32(userid);
-
-            topic.Add(topicinfo);
             return "ok";
-
         }
         [WebMethod]
-        public string DeleteCareTopic(string topicid, string userid)
+        public  string DeleteCareTopic(string topicid, string userid)
         {
             BLL.CareTopic topic = new BLL.CareTopic();
             Model.CareTopic topicinfo = new Model.CareTopic();
-            topic.Delete(Convert.ToInt32(topicid),Convert.ToInt32( userid));
-            return "qqq";
->>>>>>> origin/master
+            topic.Delete(Convert.ToInt32(topicid), Convert.ToInt32(userid));
+            return "ok";
         }
     }
 }
