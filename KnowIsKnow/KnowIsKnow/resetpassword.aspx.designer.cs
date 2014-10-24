a462d2baed3636ad7508363eb72b57a1c6ae1292
916435526 <349712821@qq.com>
2014-10-24 16:25:53 +0800

    忘记密码验证重置

--- KnowIsKnow/KnowIsKnow/resetpassword.aspx.designer.cs
+++ KnowIsKnow/KnowIsKnow/resetpassword.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace KnowIsKnow {
         protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl rspemail;
         
         /// <summary>
-        /// Email1 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl Email1;
-        
-        /// <summary>
         /// resetpwd 控件。
         /// </summary>
         /// <remarks>
