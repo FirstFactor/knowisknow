902aaa4c751ef4fcf6b9159514f5f2d07ae49ec0
hytcwentao <453167555@qq.com>
2014-10-27 14:02:44 +0800

    个人中心编辑个人资料修改

--- KnowIsKnow/KnowIsKnow/ProCenter.aspx.designer.cs
+++ KnowIsKnow/KnowIsKnow/ProCenter.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace KnowIsKnow {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rtpcared;
+        
+        /// <summary>
+        /// wtbuttonlast 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button wtbuttonlast;
     }
 }
